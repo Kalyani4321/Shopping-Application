900f822d6cf05ecd421278a7b1ad5caf48513b62
Kalyani4321 <98089499+Kalyani4321@users.noreply.github.com>
2022-04-16 11:36:37 +0530

    Repositories

---
+++ CustomerRepository.java
@@ -0,0 +1,13 @@
+ package com.example.demo.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import com.example.demo.entities.Customer;
+import com.example.demo.entities.Login;
+@Repository
+public interface CustomerRepository extends JpaRepository<Customer, Integer> {
+	 /*@Query("select c from Customer c where c.loginid = ?1")
+	 public Customer getByUserId(int id);*/
+}
